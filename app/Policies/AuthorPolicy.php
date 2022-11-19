546ef3b09f059e43dd2f49d09187782301d242ec
javad <javadimohammar@gmail.com>
2022-11-19 09:59:11 +0330

    Prepare Author policy

--- app/Policies/AuthorPolicy.php
+++ app/Policies/AuthorPolicy.php
@@ -12,23 +12,27 @@ class AuthorPolicy
 
     public function viewAny(User $user): bool
     {
-
+        return true;
     }
 
     public function view(User $user, Author $author): bool
     {
+        return $user->hasRole("admin");
     }
 
     public function create(User $user): bool
     {
+        return $user->hasRole("admin");
     }
 
     public function update(User $user, Author $author): bool
     {
+        return $user->hasRole("admin");
     }
 
     public function delete(User $user, Author $author): bool
     {
+        return $user->hasRole("admin");
     }
 
     public function restore(User $user, Author $author): bool
