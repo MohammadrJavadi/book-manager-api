f4090e0f3a37dff86d70c2ab0f7a993230b05cdd
javad <javadimohammar@gmail.com>
2022-11-19 09:38:28 +0330

    Prepare Author list page in view file

---
+++ resources/views/admin/authors/index.blade.php
@@ -0,0 +1,14 @@
+@extends("layouts.master")
+@push("title")
+    Authors
+@endpush
+@push("sub-title")
+    Authors
+@endpush
+@section("content")
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+@endsection
