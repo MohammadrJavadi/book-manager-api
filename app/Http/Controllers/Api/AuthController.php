184adde8f56fbd0926e0df895ed45a8f8a6e52d4
javad <javadimohammar@gmail.com>
2022-11-24 23:52:44 +0330

    Prepare User login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,16 +6,20 @@ use App\Exceptions\InvalidLogin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginRequest;
 use App\Http\Requests\Api\RegisterRequest;
+use App\Http\Resources\LoginResource;
 use App\Http\Resources\RegisterResource;
 use App\Repositories\Interfaces\User\UserCommandRepositoryInterface;
+use App\Repositories\Interfaces\User\UserQueryRepositoryInterface;
 
 class AuthController extends Controller
 {
     private UserCommandRepositoryInterface $command;
+    private UserQueryRepositoryInterface $query;
 
-    public function __construct(UserCommandRepositoryInterface $command)
+    public function __construct(UserCommandRepositoryInterface $command, UserQueryRepositoryInterface $query)
     {
         $this->command = $command;
+        $this->query = $query;
     }
 
     public function register(RegisterRequest $request)
@@ -30,7 +34,10 @@ class AuthController extends Controller
         if (!\Auth::attempt($request->validated()))
             throw new InvalidLogin();
 
-        return [];
+        $user = $this->query->getByPhone($request->input("phone"));
+        $token = $user->createToken("auth_token")->plainTextToken;
+
+        return new LoginResource($user, $token);
     }
 
     public function logout()
