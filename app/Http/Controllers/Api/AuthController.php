eafdabd4c1470d8190b570fcd0878e2d070dde71
javad <javadimohammar@gmail.com>
2022-11-24 19:11:23 +0330

    Prepare Auth api routes

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
