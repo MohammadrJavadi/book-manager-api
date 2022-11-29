e0c9db3180527854c3c57c02f5110f2f0310af9e
javad <javadimohammar@gmail.com>
2022-11-29 09:32:20 +0330

    Prepare Author breakpoint

---
+++ app/Http/Controllers/Api/AuthorController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Author;
+use App\Repositories\Interfaces\Author\AuthorCommandRepositoryInterface;
+use App\Repositories\Interfaces\Author\AuthorQueryRepositoryInterface;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    private AuthorCommandRepositoryInterface $command;
+    private AuthorQueryRepositoryInterface $query;
+
+    public function __construct(AuthorCommandRepositoryInterface $command, AuthorQueryRepositoryInterface $query)
+    {
+        $this->command = $command;
+        $this->query = $query;
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Author $author)
+    {
+    }
+
+    public function update(Request $request, Author $author)
+    {
+    }
+
+    public function destroy(Author $author)
+    {
+    }
+}
